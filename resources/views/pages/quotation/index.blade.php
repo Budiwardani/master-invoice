8dfc896d5e6e90fa817bc5d1b516105955c6ddbb
tyoady53 <adyprasetyo.0103@gmail.com>
2024-08-05 09:23:45 +0700

    update

--- resources/views/pages/quotation/index.blade.php
+++ resources/views/pages/quotation/index.blade.php
@@ -102,8 +102,8 @@
                                 </div>
                             </div>
                         </div>
-                        @endforeach
                         @endif
+                        @endforeach
                     @endif
                 @endif
             </div>
